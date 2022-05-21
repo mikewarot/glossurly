971e698b92016e52a2ebe95e13af11a69f345c99
Mike Warot <chezmike@gmail.com>
2022-05-21 02:20:31 -0500

    UI tweaks to Testing GUI

--- src/pascal_gui/pascal_test_gui_main.pas
+++ src/pascal_gui/pascal_test_gui_main.pas
@@ -5,7 +5,8 @@ unit pascal_test_gui_main;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls,
+  StdCtrls;
 
 type
 
@@ -13,6 +14,7 @@ type
 
   TForm1 = class(TForm)
     MainMenu1: TMainMenu;
+    Memo1: TMemo;
     MenuItem1: TMenuItem;
     MenuItem2: TMenuItem;
     MenuItem3: TMenuItem;
@@ -20,7 +22,9 @@ type
     MenuItem5: TMenuItem;
     OpenDialog1: TOpenDialog;
     Separator1: TMenuItem;
+    StatusBar1: TStatusBar;
     procedure FormCreate(Sender: TObject);
+    procedure FormResize(Sender: TObject);
     procedure MenuItem1Click(Sender: TObject);
     procedure MenuItem3Click(Sender: TObject);
     procedure MenuItem4Click(Sender: TObject);
@@ -48,7 +52,8 @@ procedure TForm1.MenuItem3Click(Sender: TObject);
 begin
   If OpenDialog1.Execute then
   begin
-
+    statusbar1.Panels[0].Text:= 'File: '+OpenDialog1.FileName;
+    Memo1.Lines.LoadFromFile(OpenDialog1.FileName);
   end;
 end;
 
@@ -60,6 +65,14 @@ end;
 procedure TForm1.FormCreate(Sender: TObject);
 begin
 
+end;
+
+procedure TForm1.FormResize(Sender: TObject);
+begin
+  StatusBar1.Panels[3].Width:= 200;
+  StatusBar1.Panels[2].Width:= 100;
+  StatusBar1.Panels[1].Width:= 100;
+  StatusBar1.Panels[0].Width:= StatusBar1.ClientWidth-400;
 end;
 
 end.
